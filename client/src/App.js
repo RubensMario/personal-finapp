c251880474f7e561be1444f780610e2754ac4e62
RubensMario <newrmmcf@gmail.com>
2020-10-01 14:02:28 -0300

    Filter  addition

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,18 @@ import { getTransactionFrom } from './api/apiService';
 import Spinner from './components/Spinner';
 import Summary from './components/Summary';
 import Filter from './components/Filter';
+import { set } from 'mongoose';
 
 export default function App() {
   // period = {id:n, date:yyyy-mm, name:'nome_mes/yyyy'}
   const [currentPeriod, setCurrentPeriod] = useState(PERIODS[0]);
-  // transações do período (mm-yyyy) selecionado
+  // transações do período (mm-yyyy) selecionado (usado se o filtro está vazio)
   /*  currentTransaction = { transactionsNumber: n, balance:R$ n, transactionsList: [transações do mês] } */
   const [currentTransactionsData, setCurrentTransactionsData] = useState({
+    transactionsList: [],
+  });
+  // Se não há filtro, seu conteúdo é preenchido com currentTransactions
+  const [filteredTransactionsData, setFilteredTransactionsData] = useState({
     transactionsList: [],
   });
 
@@ -22,7 +27,9 @@ export default function App() {
       if (!currentPeriod.date) return;
 
       const transactions = await getTransactionFrom(date);
+
       setCurrentTransactionsData(transactions);
+      setFilteredTransactionsData(transactions);
     }
 
     const currentDate = currentPeriod.date;
@@ -33,19 +40,33 @@ export default function App() {
     setCurrentPeriod(newPeriod);
   };
 
-  const handleFilterChange = (textToFilter) => {
-    const lowerCaseCategory = textToFilter.toLowerCase();
-    const filteredTransactions = currentTransactionsData.transactionsList.filter(
-      (transaction) => transaction.category.includes(lowerCaseCategory, 0)
-      // (transaction) => transaction.category === lowerCaseCategory
-    );
+  const handleFilterChange = (newTextToFilter) => {
+    // Sem texto no filtro, exibir todas as transações do mês
+    if (newTextToFilter.trim() === '') {
+      setFilteredTransactionsData(currentTransactionsData);
+    } else {
+      const lowerCaseCategory = newTextToFilter.toLowerCase();
 
-    /* Quebra por fornecer apenas transactionsList, sem as outras propriedades.
-    Isso pode ser resolvido criando-se uma função para complementar o objeto do tipo currentTransactionsData. Esse objeto pode ser usado posteriormente para a atualização do summary após o uso do filtro
-    */
-    // setCurrentTransactionsData(filteredTransactions);
-    // console.log(typeof currentTransactionsData.transactionsList[0].category);
-    console.log(filteredTransactions);
+      const filteredTransactions = currentTransactionsData.transactionsList.filter(
+        (transaction) =>
+          transaction.category.toLowerCase().includes(lowerCaseCategory)
+      );
+
+      const transactionsNumber = filteredTransactions.length;
+
+      const balance = filteredTransactions.reduce(
+        (acc, curr) => acc + curr.value,
+        0
+      );
+
+      const newFilteredTransactionsData = {
+        transactionsNumber,
+        balance,
+        transactionsList: filteredTransactions,
+      };
+
+      setFilteredTransactionsData(newFilteredTransactionsData);
+    }
   };
 
   return (
@@ -73,12 +94,9 @@ export default function App() {
           currentPeriod={currentPeriod}
           onChangePeriod={handlePeriodChange}
         />
-        <Summary currentTransactionsData={currentTransactionsData} />
-        <Filter
-          onFilterChange={handleFilterChange}
-          // textToFilter={filteredCategory}
-        />
-        <Transactions currentTransactionsData={currentTransactionsData} />
+        <Summary currentTransactionsData={filteredTransactionsData} />
+        <Filter onFilterChange={handleFilterChange} />
+        <Transactions currentTransactionsData={filteredTransactionsData} />
       </div>
     </>
   );
